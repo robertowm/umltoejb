1995b49e1f03e9ae298f2d2e39e4c0c78c21d728
robertowm <robertowm@82600952-23ea-11df-9b96-7fef40ecaa1a>
2010-05-06 21:14:50 +0000

    Tagging the 1.0 release

---
+++ tags/release-1.0/umltoejb/transformer/src/main/java/br/uff/ic/mda/transformer/core/syntax/java/Attribute.java
@@ -0,0 +1,120 @@
+package br.uff.ic.mda.transformer.core.syntax.java;
+
+/**
+ *
+ * @author robertowm
+ */
+public class Attribute implements Nonpersistent {
+
+    private String visibility;
+    private boolean staticAttribute;
+    private boolean constantAttribute;
+    private String type;
+    private String name;
+    private String value;
+
+    protected Attribute() {
+        this(null, false, false, null, null, null);
+    }
+
+    protected Attribute(String visibility, String type, String name) {
+        this(visibility, false, false, type, name, null);
+    }
+
+    protected Attribute(String visibility, String type, String name, String value) {
+        this(visibility, false, false, type, name, value);
+    }
+
+    protected Attribute(String visibility, boolean staticAttribute, boolean constantAttribute, String type, String name, String value) {
+        this.visibility = visibility;
+        this.staticAttribute = staticAttribute;
+        this.constantAttribute = constantAttribute;
+        this.type = type;
+        this.name = name;
+        this.value = value;
+    }
+
+    public String getVisibility() {
+        return this.visibility;
+    }
+
+    public Attribute setVisibility(String visibility) {
+        this.visibility = visibility;
+        return this;
+    }
+
+    public boolean isStaticAttribute() {
+        return this.staticAttribute;
+    }
+
+    public Attribute setStaticAttribute(boolean staticAttribute) {
+        this.staticAttribute = staticAttribute;
+        return this;
+    }
+
+    public boolean isConstantAttribute() {
+        return this.constantAttribute;
+    }
+
+    public Attribute setConstantAttribute(boolean constantAttribute) {
+        this.constantAttribute = constantAttribute;
+        return this;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public Attribute setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Attribute setName(String name) {
+        return setName(name, false);
+    }
+
+    public Attribute setName(String name, boolean lowerFirstLetter) {
+        this.name = (lowerFirstLetter) ? lowerFirstLetter(name) : name;
+        return this;
+    }
+
+    public String getValue() {
+        return this.value;
+    }
+
+    public Attribute setValue(String value) {
+        this.value = value;
+        return this;
+    }
+
+    protected String lowerFirstLetter(String s) {
+        return s.replaceFirst(s.substring(0,1), s.substring(0,1).toLowerCase());
+    }
+
+    @Override
+    public StringBuffer serialize() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\t");
+        if (visibility != null && !"".equals(visibility.trim())) {
+            sb.append(visibility + " ");
+        }
+        if (staticAttribute) {
+            sb.append("static ");
+        }
+        if (constantAttribute) {
+            sb.append("final ");
+        }
+        sb.append(type + " " + name);
+        if (value != null) {
+            sb.append(" = " + value);
+        }
+        sb.append(";");
+        return sb;
+    }
+
+}
---
+++ tags/release-1.0/umltoejb/transformer/src/main/java/br/uff/ic/mda/transformer/core/syntax/java/Parameter.java
@@ -0,0 +1,54 @@
+package br.uff.ic.mda.transformer.core.syntax.java;
+
+/**
+ *
+ * @author robertowm
+ */
+public class Parameter implements Nonpersistent {
+
+    private String type;
+    private String name;
+
+    protected Parameter() {
+        this(null, null);
+    }
+
+    protected Parameter(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+    public Parameter setType(String type) {
+        this.type = type;
+        return this;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Parameter setName(String name) {
+        return setName(name, false);
+    }
+
+    public Parameter setName(String name, boolean lowerFirstLetter) {
+        this.name = (lowerFirstLetter) ? lowerFirstLetter(name) : name;
+        return this;
+    }
+
+    protected String lowerFirstLetter(String s) {
+        return s.replaceFirst(s.substring(0,1), s.substring(0,1).toLowerCase());
+    }
+
+    @Override
+    public StringBuffer serialize() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(type + " " + name);
+        return sb;
+    }
+
+}
